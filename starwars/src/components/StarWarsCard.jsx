00998d2994e1659e56b17f695e486cf72580461b
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-10-11 11:09:23 -0500

    Character Data displaying in Card component

--- starwars/src/components/StarWarsCard.jsx
+++ starwars/src/components/StarWarsCard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-export default function StarWarsCard(props) {
-    console.log('Star Wars Card PROPS: ', props);
+export default function StarWarsCard( {character} ) {
+    console.log(character);
     return (
         <div>
-
+            <h3>{character.name}</h3>
+            <p>Birth Year: {character.birth_year}</p>
+            <p>Height: {character.height}</p>
+            <p>Mass: {character.mass}</p>
+            <p>Hair Color: {character.hair_color}</p>
         </div>
     )
 }
