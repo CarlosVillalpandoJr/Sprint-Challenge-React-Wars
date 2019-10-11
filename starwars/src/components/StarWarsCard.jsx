01dcf3d526287c5f3e2ea8b4438b0f4312d74ac6
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-10-11 11:17:24 -0500

    MVP Complete

--- starwars/src/components/StarWarsCard.jsx
+++ starwars/src/components/StarWarsCard.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
 
 export default function StarWarsCard( {character} ) {
     console.log(character);
     return (
-        <div>
+        <CardContainer>
             <h3>{character.name}</h3>
             <p>Birth Year: {character.birth_year}</p>
             <p>Height: {character.height}</p>
             <p>Mass: {character.mass}</p>
             <p>Hair Color: {character.hair_color}</p>
-        </div>
+        </CardContainer>
     )
 }
+
+const CardContainer = styled.div`
+border: 3px solid gold;
+border-radius: 25px;
+padding: 30px;
+margin: 25px;
+`
--- starwars/src/components/StarWarsList.jsx
+++ starwars/src/components/StarWarsList.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import StarWarsCard from './StarWarsCard';
+import styled from 'styled-components';
 
 export default function StarWarsList( {characterArray }) {
     return (
-        <div>
+        <List>
             {characterArray.map((character, index) => (
                 <StarWarsCard key={index} character={character}/>
             ))}
-        </div>
+        </List>
     )
 }
+
+
+const List = styled.div`
+margin-top: 40px;
+display: flex;
+flex-wrap: wrap;
+`
